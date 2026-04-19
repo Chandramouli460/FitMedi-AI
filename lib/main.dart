9641061d9092156cd63f7633bbc5d07f3edfb2db
Chandramouli460 <c0684629@gmail.com>
2026-04-19 09:58:13 +0530

    Update main.dart with imports for chat_page and health_page

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'chat_page.dart';
+import 'health_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FitMedi AI',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int index = 0;
+
+  final pages = const [
+    ChatPage(),
+    HealthPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (i) => setState(() => index = i),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "AI Chat"),
+          BottomNavigationBarItem(icon: Icon(Icons.health_and_safety), label: "Health"),
+        ],
+      ),
+    );
+  }
+}
